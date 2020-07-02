40447fbcba0652b67b5b16fb5ce9bfbdac5f162d
Zain Afzal <zain.afzal@rise.cx>
2020-07-02 11:15:14 +1000

    Add Logic to determine the winnner

--- src/index.js
+++ src/index.js
@@ -28,6 +28,11 @@ function Square(props) {
     }
     handleClick(i){
       const squares = this.state.squares.slice();
+
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      }
+      
       squares[i] = this.state.xIsNext ? 'X' : '0';
       this.setState({
         squares:squares,
@@ -36,7 +41,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+      const winner = calculateWinner(this.state.squares)
+      let status;
+      if (winner){
+        status = 'Winner' + winner;
+      } 
+      else{
+        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : '0')
+      }
   
       return (
         <div>
@@ -83,4 +95,23 @@ function Square(props) {
     <Game />,
     document.getElementById('root')
   );
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
